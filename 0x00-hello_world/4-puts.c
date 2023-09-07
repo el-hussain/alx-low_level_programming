3db156339c5d3a883f6a104aa18ae024e5c852a2
el-hussain <hussainisuleimanahmad@gmail.com>
2023-09-07 12:07:47 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-    
+
+/**
+ * main - This is the main function and ots void
+ *
+ * Return: its return the value zero
+ */
+
     int main (void){
-        puts("Programming is like building a multilingual puzzle");
+        puts("\"programming is like building a multilingual puzzle");
         return (0);        
     }
