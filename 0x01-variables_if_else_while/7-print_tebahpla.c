8c13a21709e5b7fa6a4413946dd8f85a2f28570f
el-hussain <hussainisuleimanahmad@gmail.com>
2023-09-11 14:14:54 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the lowercase alphabets in reverse.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char low_alphabet;
+
+for (low_alphabet = 'z'; low_alphabet >= 'a'; low_alphabet--)
+{
+putchar(low_alphabet);
+}
+putchar('\n');
+
+return (0);
+}
