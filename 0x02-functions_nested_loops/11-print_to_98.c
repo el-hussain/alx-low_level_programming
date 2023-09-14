61a6998adaaff74f9a58e657c2473e0ea3938712
el-hussain <hussainisuleimanahmad@gmail.com>
2023-09-13 17:34:49 -0700

    98 Battery Street

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - Prints all N numbers from input to 98,
+ *               separated by a comma followed by a space for each.
+ * @n: Begin counting from this number.
+ */
+void print_to_98(int n)
+{
+if (n >= 98)
+{
+while (n > 98)
+{
+printf("%d, ", n--);
+}
+printf("%d\n", n);
+}
+else
+{
+while (n < 98)
+{
+printf("%d, ", n++);
+}
+printf("%d\n", n);
+}
+}
