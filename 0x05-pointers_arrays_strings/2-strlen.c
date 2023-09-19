74299dd7afb2dc2ae823f30e5cdc71f5d2e21c6d
el-hussain <hussainisuleimanahmad@gmail.com>
2023-09-19 01:05:15 -0700

    This report

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+/*
+ * File: 2-strlen.c
+ * Author: Hussaini suleiman
+ */
+
+#include "main.h"
+
+/**
+ * _strlen - Counts the length of a string.
+ * @s: Each character in the string.
+ *
+ * Return: Length of string.
+ */
+	int _strlen(char *s)
+	{
+	int len = 0;
+	while (*s != '\0')
+	{
+	len++;
+	s++;
+	}
+	return (len);
+	}
+
