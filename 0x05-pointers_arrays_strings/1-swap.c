f3b8910c8cf3300aabf7ea82f4f2b853ed627c36
el-hussain <hussainisuleimanahmad@gmail.com>
2023-09-19 00:53:14 -0700

    Don't swap horses

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+/*
+ * File: 1-swap.c
+ * Author: Hussaini suleiman
+ */
+
+#include "main.h"
+
+/**
+ * swap_int - Swaps the values of two integers.
+ * @a: First integer.
+ * @b: Second integer.
+ */
+	void swap_int(int *a, int *b)
+	{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+	}
