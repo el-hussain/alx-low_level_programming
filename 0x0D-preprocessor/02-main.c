ca352a4e8ecab4b51f6fc9882088012ec282c4b4
el-hussain <hussainisuleimanahmad@gmail.com>
2023-10-14 13:47:39 -0700

    file name

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,16 @@
+/*
+ * File: 2-main.c
+ * Auth: Hussaini Suleiman
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: 0
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
