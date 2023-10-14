3cc58f74ca1e4440ce7aff8ef89eaa3765a182e6
el-hussain <hussainisuleimanahmad@gmail.com>
2023-10-14 13:40:29 -0700

    Object-like Macro

---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: Hussaini Suleiman
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x0D-preprocessor directory.
+ */
+
+int _putchar(char c);
+
+#endif
